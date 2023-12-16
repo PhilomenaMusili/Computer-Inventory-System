bb0c43de8a418b6f4e0d9b05e39bf6b831d32c78
PhilomenaMusili <philomenamusili@gmail.com>
2023-12-16 10:10:28 +0300

    Commit

---
+++ djangoapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Computer
+from .forms import ComputerForm
+
+class ComputerAdmin(admin.ModelAdmin):
+    list_display = ["computer_name", "IP_address", "MAC_address", "users_name"]
+    form = ComputerForm
+    list_filter = ['computer_name', 'IP_address']
+    search_fields = ['computer_name', 'IP_address','MAC_address']
+
+admin.site.register(Computer, ComputerAdmin)
