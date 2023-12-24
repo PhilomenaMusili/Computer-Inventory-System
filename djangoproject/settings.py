31e171b13307e98e428a9303e7db39804f3d6b1c
PhilomenaMusili <philomenamusili@gmail.com>
2023-12-25 00:37:29 +0300

    updates

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'djangoapp',
 )
 
@@ -101,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
